3eda4c1769dbe314b97a125b5c0a3907ff681ad9
ZiyaoLi <leeeezy@126.com>
2018-07-16 23:05:14 -0400

    parameters moved out

--- example.py
+++ example.py
@@ -5,14 +5,25 @@ from group import Louvain, groups2inv_index, pure_override_nodes
 import numpy as np
 from numpy.linalg import norm
 
+K_SIZE = 200
+DIMENSION = 100
+VERBOSE = 1
+GROUP_IGNORE = 1
+
 net = Graph('wiki.txt', typ=1)
-k_set = sample(net, k=200, method='deg_deter')
 grouping_model = Louvain(net)
 groups = grouping_model.execute()
+group_sizes = [len(t) for t in groups]
+inv_index_original = groups2inv_index(groups, net.nVertices)
+sizes_index = [group_sizes[t - 1] for t in inv_index_original]
+
+# k_set = sample(net, k=K_SIZE, method='deg_deter')
+k_set = sample(net, k=K_SIZE, method='deg^2|group_prob', size_index=sizes_index)
+
 inv_index = groups2inv_index(groups, net.nVertices, k_set)
 pure_override_nodes(groups, inv_index)
 groups = [k_set] + groups
-model = Optimizer(net, groups, dim=100)
+model = Optimizer(net, groups, dim=DIMENSION)
 vecs_w = []
 vecs_c = []
 
@@ -20,7 +31,9 @@ all_idx = []
 for t in range(len(groups)):
     print("%d / %d, number of vertices = %d..."
           % (t + 1, len(groups), len(groups[t])))
-    w, c = model.train(t, verbose=2)
+    if len(groups[t]) <= GROUP_IGNORE:
+        continue
+    w, c = model.train(t, verbose=VERBOSE)
     vecs_w.append(w)
     vecs_c.append(c)
     all_idx += groups[t]
@@ -43,8 +56,8 @@ print("Original - %.4f, delta - %.4f, percentage - %.4f"
 
 # a SVD implementation to exam how good is the result
 u, d, v = np.linalg.svd(original)
-w_svd = (u[:, :100] * np.sqrt(d[:100])).T
-c_svd = (v.T[:, :100] * np.sqrt(d[:100])).T
+w_svd = (u[:, :DIMENSION] * np.sqrt(d[:DIMENSION])).T
+c_svd = (v.T[:, :DIMENSION] * np.sqrt(d[:DIMENSION])).T
 reconstruct_svd = w_svd.T @ c_svd
 delta_svd = original - reconstruct_svd
 t_svd = norm(delta_svd, 'fro')
