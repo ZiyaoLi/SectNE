cba66a159c53e5a16fa594ae20f0e624b3ce1f90
ZiyaoLi <leeeezy@126.com>
2018-07-18 14:40:39 -0400

    better output name implementation

--- example.py
+++ example.py
@@ -3,19 +3,32 @@ from sample import sample
 from train import Optimizer
 from group import Louvain, groups2inv_index, pure_override_nodes
 import numpy as np
+import train
 from numpy.linalg import norm
 
-K_SIZE = 400
+K_SIZE = 200
 DIMENSION = 100
-VERBOSE = 2
+VERBOSE = 1
 GROUP_IGNORE = 1
-SAMPLE_METHOD = 'deg^2|group_prob'
+SAMPLE_METHOD = 'uniform'
 RANDOM_GROUPING = True
 
-MATRIX_TO_FILE = False
-FILE_NAME = 'wiki_200k_100d_l0.8_e0.1_theta1_prob.csv'
+DATASET = 'wiki'
+DATADIR = 'data\\'
+ABS_DELTA_MATRIX_TO_FILE = True
+UNIQUE_NAME = 'cleared-null-entries'
+FILE_NAME = 'results\\' + '_'.join([
+    DATASET,
+    'k=%s' % K_SIZE,
+    'd=%s' % DIMENSION,
+    'sample=%s' % SAMPLE_METHOD,
+    'lambda=%.2f' % train.LAMBDA,
+    'eta=%.2f' % train.ETA,
+    'max-iter=%04d' % train.MAX_ITER,
+    '%s%.3f' % (UNIQUE_NAME, np.random.rand())
+]) + '.csv'
 
-net = Graph('wiki.txt', typ=1)
+net = Graph(DATADIR + DATASET + '\\links.txt', typ=1)
 grouping_model = Louvain(net)
 groups = grouping_model.execute(rand=RANDOM_GROUPING)
 group_sizes = [len(t) for t in groups]
@@ -53,10 +66,10 @@ original = net.calc_matrix(all_idx, all_idx)
 
 # evaluate the reconstruction performance
 delta = original - reconstruct
-abs_delta = abs(delta)
-if MATRIX_TO_FILE:
+abs_delta = abs(delta)  # * (original != 0)
+if ABS_DELTA_MATRIX_TO_FILE:
     np.savetxt(FILE_NAME, abs_delta, '%.6e', ',', '\n')
-t = norm(delta, 'fro')
+t = norm(abs_delta, 'fro')
 tt = norm(original, 'fro')
 print("Original - %.4f, delta - %.4f, percentage - %.4f"
       % (tt, t, t / tt))
@@ -66,7 +79,7 @@ u, d, v = np.linalg.svd(original)
 w_svd = (u[:, :DIMENSION] * np.sqrt(d[:DIMENSION])).T
 c_svd = (v.T[:, :DIMENSION] * np.sqrt(d[:DIMENSION])).T
 reconstruct_svd = w_svd.T @ c_svd
-delta_svd = original - reconstruct_svd
+delta_svd = (original - reconstruct_svd)  # * (original != 0)
 t_svd = norm(delta_svd, 'fro')
 print("Original - %.4f, delta - %.4f, percentage - %.4f"
       % (tt, t_svd, t_svd / tt))
