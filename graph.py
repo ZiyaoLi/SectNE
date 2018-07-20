4dc54e069417cc612e5a3d275553a10577e564d0
ZiyaoLi <leeeezy@126.com>
2018-07-20 14:24:42 -0400

    major adjustment: expecting directed graph by default, and faster matrix-fetching implemented.

--- graph.py
+++ graph.py
@@ -1,46 +1,38 @@
 import numpy as np
 
 
-class Proximity:
-
-    def __init__(self):
-        self.prox = set()
-        self.degree = 0
-
-    def __len__(self):
-        return self.degree
-
-    def __contains__(self, item):
-        return item in self.prox
-
-    def add(self, vid):
-        self.degree += 1
-        self.prox.add(vid)
-
-    def __mul__(self, other):
-        assert isinstance(other, Proximity)
-        return len(self.prox.intersection(other.prox))
+LAMBDA = 0.5
 
 
-class VSet:
+def inverse_index(idx):
+    rst = {}
+    for i, item in enumerate(idx):
+        rst[item] = i
+    return rst
 
-    def __init__(self, net, indices):
-        self.num = len(indices)
-        self.len = net.nVertices
-        self.vertices = net.fetch_subset(indices)
 
-    def __mul__(self, other):
-        assert isinstance(other, VSet)
-        rst = np.zeros((self.num, other.num))
-        for i, vi in enumerate(self.vertices):
-            for j, vj in enumerate(other.vertices):
-                rst[i, j] = vi * vj
-        return rst
+class Vertex:
+    def __init__(self, vid):
+        self.id = vid
+        self.in_prox = set()
+        self.out_prox = set()
+        self.in_degree = 0
+        self.out_degree = 0
+
+    def add_out(self, vid):
+        if vid not in self.out_prox:
+            self.out_degree += 1
+            self.out_prox.add(vid)
+
+    def add_in(self, vid):
+        if vid not in self.in_prox:
+            self.in_degree += 1
+            self.in_prox.add(vid)
 
 
 class Graph:
 
-    def __init__(self, filename, sep='\t', typ=0):
+    def __init__(self, filename, sep='\t', typ='dir', verbose=True):
         self.vertices = []
         self.nVertices = 0
         self.nEdges = 0
@@ -48,49 +40,103 @@ class Graph:
         s = f.readline()
         while len(s):
             pair = s.strip().split(sep)
-            vid1 = int(pair[0])
-            vid2 = int(pair[1])
-            self.add_edge(vid1, vid2, typ)
+            vid_in = int(pair[0])
+            vid_out = int(pair[1])
+            self.add_edge(vid_in, vid_out, typ)
             s = f.readline()
+            if verbose:
+                if not self.nEdges % 5e6:
+                    print('%d edges processed...' % self.nEdges)
+        self.vid2newVid_mapping = {}
+        self.newVid2vid_mapping = {}
+        if verbose:
+            print('reducing empty indices...')
+        self.reduce()
 
     def add_vertex(self, vid):
         if self.nVertices > vid:
             if self.vertices[vid] is None:
-                self.vertices[vid] = Proximity()
+                self.vertices[vid] = Vertex(vid)
         else:
-            for t in range(vid + 1 - self.nVertices):
-                self.vertices.append(None)
-            self.vertices[vid] = Proximity()
+            self.vertices += [None] * (vid + 1 - self.nVertices)
+            self.vertices[vid] = Vertex(vid)
             self.nVertices = vid + 1
 
-    def add_edge(self, vinid, voutid, typ=0):
-        # typ - 0 for direct, 1 for undirect
-        self.add_vertex(vinid)
-        self.add_vertex(voutid)
-        self.vertices[vinid].add(voutid)
+    def add_edge(self, vid_in, vid_out, typ='dir'):
+        # typ - 'dir' for directed, 'undir' for undirected
+        self.add_vertex(vid_in)
+        self.add_vertex(vid_out)
+        self.vertices[vid_in].add_out(vid_out)
+        self.vertices[vid_out].add_in(vid_in)
         self.nEdges += 1
-        if typ == 1:
-            self.vertices[voutid].add(vinid)
-            self.nEdges += 1
+        if typ == 'undir':
+            self.add_edge(vid_out, vid_in, 'undir')
 
-    def fetch_prox(self, vid):
-        return self.vertices[vid].prox
+    def fetch_prox(self, vid, typ='out'):
+        if typ == 'out':
+            return self.vertices[vid].out_prox
+        else:
+            return self.vertices[vid].in_prox
 
-    def fetch_subset(self, indices):
-        rst = []
-        for t in indices:
-            rst.append(self.vertices[t])
+    def fetch_prox_mat(self, idx_in, idx_out):
+        rst = np.zeros((len(idx_in), len(idx_out)))
+        for row_id, vid_in in enumerate(idx_in):
+            for col_id, vid_out in enumerate(idx_out):
+                if vid_out in self.fetch_prox(vid_in, 'out'):
+                    rst[row_id, col_id] = 1
         return rst
 
-    def fetch_proxmat(self, indicesin, indicesout):
-        rst = np.zeros((len(indicesin), len(indicesout)))
-        for i, vinid in enumerate(indicesin):
-            for j, voutid in enumerate(indicesout):
-                if voutid in self.vertices[vinid]:
-                    rst[i, j] = 1
-        return rst
+    def calc_matrix(self, idx_in, idx_out):
+        prox_mat = self.fetch_prox_mat(idx_in, idx_out)
+        secondary_mat = np.zeros([len(idx_in), len(idx_out)])
+        if len(idx_in) <= len(idx_out):
+            vid2colid_map = inverse_index(idx_out)
+            for row_id, vid in enumerate(idx_in):
+                for first_neighbor in self.fetch_prox(vid, 'out'):
+                    for second_neighbor in self.fetch_prox(first_neighbor, 'out'):
+                        try:
+                            col_id = vid2colid_map[second_neighbor]
+                            secondary_mat[row_id, col_id] += 1
+                        except KeyError:
+                            pass
+        else:
+            vid2rowid_map = inverse_index(idx_in)
+            for col_id, vid in enumerate(idx_out):
+                for first_neighbor in self.fetch_prox(vid, 'in'):
+                    for second_neighbor in self.fetch_prox(first_neighbor, 'in'):
+                        try:
+                            row_id = vid2rowid_map[second_neighbor]
+                            secondary_mat[row_id, col_id] += 1
+                        except KeyError:
+                            pass
+        return prox_mat + LAMBDA * secondary_mat
 
-    def calc_matrix(self, indicesin, indicesout):
-        rst = self.fetch_proxmat(indicesin, indicesout)
-        rst += 0.5 * (VSet(self, indicesin) * VSet(self, indicesout))
-        return rst
+    def _set_reduce_mapping(self):
+        top = 0
+        for vid in range(self.nVertices):
+            if self.vertices[vid] is not None:
+                self.vid2newVid_mapping[vid] = top
+                self.newVid2vid_mapping[top] = vid
+                top += 1
+
+    def _reduce(self, verbose=True):
+        self._set_reduce_mapping()
+        clean_vertices = []
+        for vid, newVid in self.vid2newVid_mapping.items():
+            new_vertex = Vertex(newVid)
+            for out_neighbor in self.fetch_prox(vid, 'out'):
+                new_vertex.add_out(self.vid2newVid_mapping[out_neighbor])
+            for in_neighbor in self.fetch_prox(vid, 'in'):
+                new_vertex.add_in(self.vid2newVid_mapping[in_neighbor])
+            clean_vertices.append(new_vertex)
+            self.nEdges += new_vertex.out_degree
+            if verbose:
+                if not newVid % 1e5:
+                    print('%d vertices reduced...' % newVid)
+        self.vertices = clean_vertices
+        self.nVertices = len(clean_vertices)
+
+
+
+
+
